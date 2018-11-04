30d8274016df775884804aa820768b77b37129e5
jesus Barajas Camacho <jesusbarcam@gmail.com>
2018-11-04 19:21:40 +0100

    Gallery extends version

--- public/polyfills.js
+++ public/polyfills.js
@@ -5473,7 +5473,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/jesusbarcam/Developer/Projects/bioclimatic/repository/bioclimatic-house/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/Rawdog/Developer/Projects/BioclimaticBigHouse/Repository/bioclimatic-house/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
